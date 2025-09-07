5b94ca016f3a3b4e6cab636ad3c67c3995e8c3ec
Marcelo F Andrade <mfandrade@gmail.com>
2025-09-06 21:14:57 -0400

    feat: screenshot properly working with $mod-print and $mod-ctrl-print

--- bin/screenshot.sh
+++ bin/screenshot.sh
@@ -1,20 +1,29 @@
 #!/bin/sh
 
-IMAGENAME="screenshot.$(date '+%F.%T' | tr ':' '-').png"
+MODE="${1:-screen}"
 DIR="${XDG_PICTURES_DIR:-$HOME/dirs/Images}"
-file_path="$DIR/$IMAGENAME"
-
+IMAGENAME="screenshot.$(date '+%F,%T,%N').png"
+filepath="$DIR/$IMAGENAME"
 symlink="$DIR/latest-screenshot.png"
+
+case "$MODE" in
+  screen)
+    grimshot --notify save screen "$filepath"
+    ;;
+  area)
+    grimshot --notify savecopy anything "$filepath"
+    ;;
+  *)
+    echo "$0: invalid mode '$MODE'. Use 'screen' or 'area'."
+    exit 1
+    ;;
+esac
+
 sound="$HOME/dirs/Sounds/screenshot.wav"
-
-grim "$file_path"
-
 if test $? -eq 0; then
-  ln -sf "$file_path" "$symlink"
+  ln -sf "$filepath" "$symlink"
 
   if test -f "$sound"; then
-    aplay "$sound" &
+    aplay -q "$sound" &
   fi
-
-  notify-send "Screenshot captured" "Image saved as $IMAGENAME" -i "camera"
 fi

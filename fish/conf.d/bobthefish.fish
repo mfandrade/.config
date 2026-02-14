2dce58be1820c46e69b30a16d6468ed98a353232
Marcelo F Andrade <mfandrade@gmail.com>
2026-02-14 20:25:48 -0300

    refactor(fish): move theme related commands to bobthefish.fish

--- fish/conf.d/bobthefish.fish
+++ fish/conf.d/bobthefish.fish
@@ -1,2 +1,4 @@
+set -g theme_nerd_fonts yes
 set -g theme_newline_cursor yes
 set -g theme_display_vi no
+set -g theme_display_ruby no
--- fish/config.fish
+++ fish/config.fish
@@ -1,21 +1,14 @@
 # ~/.config/fish/config.fish
 
-set -gx PAGER "bat -p"
+set -gx PAGER 'bat -p'
 set -gx MANPAGER 'sh -c "col -b | bat -l man -p"'
 set -gx MANROFFOPT -c
 set --path PATH $HOME/bin $PATH
 
 if status is-interactive
-
     set -q SSH_AUTH_SOCK; or eval (ssh-agent -c)
-
-    set -x EZA_ICONS_AUTO yes
-
+    set -gx EZA_ICONS_AUTO yes
     set -gx RIPGREP_CONFIG_PATH "$HOME/.config/ripgrep/config"
-
-    set -g theme_nerd_fonts yes
-    set -g theme_display_vi yes
-    set -g theme_display_ruby no
     fish_vi_key_bindings
 
 end
